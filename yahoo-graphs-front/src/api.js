5cd06a6c2b249001e8a52b8ffa865decf41ad758
Rafael Melo <rafaeltmelo03@gmail.com>
2025-04-30 18:08:11 -0300

    Adicionando o Readme

--- yahoo-graphs-front/src/api.js
+++ yahoo-graphs-front/src/api.js
@@ -1,4 +1,4 @@
 export const getStockHistory = async (symbol) => {
-  const response = await fetch(`http://localhost:5259/api/yahoo/${symbol}`);
+  const response = await fetch(`http://localhost:5000/api/yahoo/${symbol}`);
   return await response.json();
 };
